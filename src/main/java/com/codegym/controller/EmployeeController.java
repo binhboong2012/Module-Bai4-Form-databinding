7a0656f3b84ad7062890585ad69b59336572e385
binhboong2012 <binhcauduongbo@gmail.com>
2022-03-22 13:38:40 +0700

    Form datab

---
+++ src/main/java/com/codegym/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.codegym.controller;
+
+import com.codegym.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("employee/")
+public class EmployeeController {
+    @RequestMapping(value = "showForm", method = RequestMethod.GET)
+    public String showForm(ModelMap model) {
+        model.addAttribute("employee", new Employee());
+        return "employee/create";
+    }
+
+    @RequestMapping(value = "/addEmployee", method = RequestMethod.POST)
+    public String submit(@ModelAttribute("employee") Employee employee, ModelMap model) {
+        return "employee/info";
+    }
+}
